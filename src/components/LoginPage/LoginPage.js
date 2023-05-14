556fbba62ccf1471bca30c8eecdd8012de89f5d4
hhyanghh <yanghh711@naver.com>
2023-05-14 18:16:42 +0900

    login 처리

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,7 +1,146 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom";
+import { useForm } from "react-hook-form";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [loading, setLoading] = useState(false);
+  const auth = getAuth();
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      // 로그인 성공 처리
+      console.log("로그인 성공:", userCredential.user);
+
+      setLoading(false);
+    } catch (error) {
+      alert(error.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Login
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form
+            className="space-y-6"
+            action="#"
+            method="POST"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="이메일을 입력하세요."
+                  autoComplete="email"
+                  className="p-2 block w-full rounded-md border py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  {...register("email", {
+                    required: "이메일은 필수 입력 항목입니다.",
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: "유효한 이메일 주소를 입력해주세요.",
+                    },
+                  })}
+                />
+                {errors.email && (
+                  <p className="relative text-red pl-4 mt-1 text-sm">
+                    <span className="absolute left-0 top-0 inline">⚠ </span>
+                    {errors.email.message}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  placeholder="비밀번호를 입력하세요."
+                  className="p-2 block w-full rounded-md border py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  {...register("password", {
+                    required: "비밀번호는 필수 입력 항목입니다.",
+                    minLength: {
+                      value: 6,
+                      message: "비밀번호는 6자 이상입니다.",
+                    },
+                  })}
+                />
+                {errors.password && (
+                  <p className="relative text-red pl-4 mt-1 text-sm">
+                    <span className="absolute left-0 top-0 inline">⚠ </span>
+                    {errors.password.message}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                disabled={loading}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            아이디가 없다면?
+            <Link
+              to="register"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 ml-1"
+            >
+              회원가입 하기
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LoginPage;
