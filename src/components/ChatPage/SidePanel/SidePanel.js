b77e9991ecc38e39cda9bcfb9e28ffd896ff7ecb
hhyanghh <yanghh711@naver.com>
2023-05-23 19:40:37 +0900

    채팅페이지 UI

--- src/components/ChatPage/SidePanel/SidePanel.js
+++ src/components/ChatPage/SidePanel/SidePanel.js
@@ -1,7 +1,26 @@
 import React from "react";
+import UserPanel from "./UserPanel";
+import Favorited from "./Favorited";
+import ChatRooms from "./ChatRooms";
+import DirectMessages from "./DirectMessages";
 
 function SidePanel() {
-  return <div>SidePanel</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "#7B83EB",
+        padding: "2rem",
+        minHeight: "100vh",
+        color: "#ffffff",
+        minWidth: "275px",
+      }}
+    >
+      <UserPanel />
+      <Favorited />
+      <ChatRooms />
+      <DirectMessages />
+    </div>
+  );
 }
 
 export default SidePanel;
