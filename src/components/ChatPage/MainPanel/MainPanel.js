568d9d3c9c9f778691318a4628843ae21cdf56e1
hhyanghh <yanghh711@naver.com>
2023-05-23 19:28:04 +0900

    채팅페이지 UI

---
+++ src/components/ChatPage/MainPanel/MainPanel.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+// setState가 잘 안되서
+export class MainPanel extends Component {
+  render() {
+    return <div>MainPanel</div>;
+  }
+}
+
+export default MainPanel;
