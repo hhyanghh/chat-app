387561a36d7a78aa06bbbe8aa15060143102de7e
hhyanghh <yanghh711@naver.com>
2023-05-23 19:54:29 +0900

    UserPanel - UI, redux에서 user 정보 가져오기

--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { BsFillChatDotsFill } from "react-icons/bs";
+import Dropdown from "react-bootstrap/Dropdown";
+import Image from "react-bootstrap/Image";
+import { useSelector } from "react-redux";
 
 function UserPanel() {
-  return <div>UserPanel</div>;
+  // redex store에서 유저 정보 가져오기
+  const user = useSelector((state) => state.user.currentUser);
+  return (
+    <div>
+      <h3>
+        <BsFillChatDotsFill /> Chat App
+      </h3>
+
+      <div style={{ display: "flex", marginBottom: "1rem" }}>
+        <Image
+          src={user && user.photoURL}
+          roundedCircle
+          style={{ width: "30px", height: "30px", marginTop: "3px" }}
+        />
+        <Dropdown>
+          <Dropdown.Toggle
+            id="dropdown-basic"
+            style={{ background: "transparent", border: "0px" }}
+          >
+            {user.displayName}
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">프로필 사진 변경</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">로그아웃</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </div>
+  );
 }
 
 export default UserPanel;
