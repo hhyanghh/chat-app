fa33537ade02d153d4e4236d5eb703360cc8b945
hhyanghh <yanghh711@naver.com>
2023-05-27 15:27:09 +0900

    Message Header UI

--- src/components/ChatPage/MainPanel/MessageHeader.js
+++ src/components/ChatPage/MainPanel/MessageHeader.js
@@ -1,7 +1,103 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Image } from "react-bootstrap";
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
+import { BsSearch, BsUnlock, BsHeart } from "react-icons/bs";
+import Accordion from "react-bootstrap/Accordion";
+import { useAccordionButton } from "react-bootstrap/AccordionButton";
+import Card from "react-bootstrap/Card";
+
+function CustomToggle({ children, eventKey }) {
+  const decoratedOnClick = useAccordionButton(eventKey, () =>
+    console.log("totally custom!")
+  );
+
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: "pink", padding: "2px" }}
+      onClick={decoratedOnClick}
+    >
+      {children}
+    </button>
+  );
+}
 
 function MessageHeader() {
-  return <div>MessageHeader</div>;
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "170px",
+        // border: ".2rem solid #ececec",
+        backgroundColor: "#ececec70",
+        borderRadius: "16px",
+        padding: "1rem",
+        marginBottom: "1rem",
+      }}
+    >
+      <Container>
+        <Row>
+          <Col>
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">
+                <BsSearch />
+              </InputGroup.Text>
+              <Form.Control
+                placeholder="Search Messages"
+                aria-label="Search"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2 style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+              <BsUnlock />
+              <div>Chat Room Name</div>
+              <BsHeart style={{ marginLeft: "8px" }} />
+            </h2>
+          </Col>
+          <Col>
+            <Image src="" />
+            <p>User Name</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Accordion>
+              <Card>
+                <Card.Header>
+                  <CustomToggle eventKey="0">
+                    Chat Room Description
+                  </CustomToggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>Hello! I'm the body</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </Col>
+          <Col>
+            <Accordion>
+              <Card>
+                <Card.Header>
+                  <CustomToggle eventKey="0">Count Posts</CustomToggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>Hello! I'm the body</Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default MessageHeader;
