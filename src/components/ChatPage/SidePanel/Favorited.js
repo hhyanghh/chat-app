1683b275ec4979e7c6b314c88c23f8ea92d631d1
hhyanghh <yanghh711@naver.com>
2023-05-29 20:12:56 +0900

    Favorited 기능 완성

--- src/components/ChatPage/SidePanel/Favorited.js
+++ src/components/ChatPage/SidePanel/Favorited.js
@@ -1,7 +1,121 @@
-import React from "react";
+import React, { Component } from "react";
+import { FaRegSmileBeam } from "react-icons/fa";
+import { connect } from "react-redux";
+import {
+  setCurrentChatRoom,
+  setPrivateChatRoom,
+} from "../../../redux/actions/chatRoom_action";
 
-function Favorited() {
-  return <div>Favorited</div>;
+import {
+  child,
+  getDatabase,
+  ref,
+  onChildAdded,
+  onChildRemoved,
+  off,
+  onValue,
+} from "firebase/database";
+
+export class Favorited extends Component {
+  state = {
+    favoritedChatRooms: [],
+    activeChatRoomId: "",
+    userRef: ref(getDatabase(), "users"),
+  };
+
+  componentDidMount() {
+    if (this.props.user) {
+      this.addListeners(this.props.user.uid);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.user) {
+      this.removeListener(this.props.user.uid);
+    }
+  }
+
+  removeListener = (userId) => {
+    const { userRef } = this.state;
+    off(child(userRef, `${userId}/favoried`));
+  };
+
+  addListeners = (userId) => {
+    const { userRef } = this.state;
+
+    onValue(child(userRef, `${userId}/favorited`), (DataSnapshot) => {
+      const favoritedChatRooms = [];
+      DataSnapshot.forEach((childSnapshot) => {
+        const chatRoom = { id: childSnapshot.key, ...childSnapshot.val() };
+        favoritedChatRooms.push(chatRoom);
+      });
+      this.setState({ favoritedChatRooms });
+    });
+
+    onChildAdded(child(userRef, `${userId}/favorited`), (DataSnapshot) => {
+      const favoritedChatRoom = { id: DataSnapshot.key, ...DataSnapshot.val() };
+      this.setState({
+        favoritedChatRooms: [
+          ...this.state.favoritedChatRooms,
+          favoritedChatRoom,
+        ],
+      });
+    });
+
+    onChildRemoved(child(userRef, `${userId}/favorited`), (DataSnapshot) => {
+      const chatRoomToRemove = { id: DataSnapshot.key, ...DataSnapshot.val() };
+      const filteredChatRooms = this.state.favoritedChatRooms.filter(
+        (chatRoom) => {
+          return chatRoom.id !== chatRoomToRemove.id;
+        }
+      );
+      this.setState({ favoritedChatRooms: filteredChatRooms });
+    });
+  };
+
+  changeChatRoom = (room) => {
+    this.props.dispatch(setCurrentChatRoom(room));
+    this.props.dispatch(setPrivateChatRoom(false));
+    this.setState({ activeChatRoomId: room.id });
+  };
+
+  renderFavoritedChatRooms = (favoritedChatRooms) =>
+    favoritedChatRooms.length > 0 &&
+    favoritedChatRooms.map((chatRoom) => (
+      <li
+        key={chatRoom.id}
+        onClick={() => this.changeChatRoom(chatRoom)}
+        style={{
+          backgroundColor:
+            chatRoom.id === this.state.activeChatRoomId && "#ffffff45",
+          cursor: "pointer",
+        }}
+      >
+        # {chatRoom.name}
+      </li>
+    ));
+
+  render() {
+    const { favoritedChatRooms } = this.state;
+    console.log(favoritedChatRooms);
+    return (
+      <div>
+        <span style={{ display: "flex", alignItems: "center" }}>
+          <FaRegSmileBeam style={{ marginRight: "3px" }} />
+          FAVORITED ({favoritedChatRooms.length})
+        </span>
+        <ul style={{ listStyleType: "none", padding: "0" }}>
+          {this.renderFavoritedChatRooms(favoritedChatRooms)}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default Favorited;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user.currentUser,
+  };
+};
+
+export default connect(mapStateToProps)(Favorited);
