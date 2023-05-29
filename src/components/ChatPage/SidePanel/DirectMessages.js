d441e6442f7324828a792745e496811a52cb1e0e
hhyanghh <yanghh711@naver.com>
2023-05-29 12:25:26 +0900

    사이드패널 DM UI

--- src/components/ChatPage/SidePanel/DirectMessages.js
+++ src/components/ChatPage/SidePanel/DirectMessages.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 
-function DirectMessages() {
-  return <div>DirectMessages</div>;
+import { BiWinkSmile } from "react-icons/bi";
+
+export class DirectMessages extends Component {
+  renderDirectMessages = () => {
+    // list 그리기 (가입된 다른 유저들)
+  };
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <BiWinkSmile style={{ marginRight: 3 }} />
+          DIRECT MESSAGES (1)
+        </div>
+
+        <ul style={{ listStyleType: "none", padding: 0 }}>
+          {this.renderDirectMessages()}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default DirectMessages;
