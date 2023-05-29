9d59969c8ab9f10217f301bafa72a94f472c3369
hhyanghh <yanghh711@naver.com>
2023-05-29 18:34:03 +0900

    messageRef 정리해주기

--- src/components/ChatPage/SidePanel/ChatRooms.js
+++ src/components/ChatPage/SidePanel/ChatRooms.js
@@ -40,6 +40,10 @@ export class ChatRooms extends Component {
 
   componentWillUnmount() {
     off(this.state.chatRoomsRef);
+
+    this.state.chatRooms.forEach((chatRoom) => {
+      off(child(this.state.messagesRef, chatRoom.id));
+    });
   }
 
   setFirstChatRoom = () => {
@@ -165,6 +169,20 @@ export class ChatRooms extends Component {
     this.props.dispatch(setCurrentChatRoom(room));
     this.props.dispatch(setPrivateChatRoom(false));
     this.setState({ activeChatRoomId: room.id });
+    this.clearNotifications();
+  };
+
+  clearNotifications = () => {
+    let index = this.state.notifications.findIndex(
+      (notification) => notification.id === this.props.chatRoom.id
+    );
+    if (index !== -1) {
+      let updatedNotifications = [...this.state.notifications];
+      updatedNotifications[index].lastKnowTotal =
+        this.state.notifications[index].total;
+      updatedNotifications[index].count = 0;
+      this.setState({ notifications: updatedNotifications });
+    }
   };
 
   getNotificationCount = (room) => {
